baf5ee081c4d176896443286afd894ccd9b1b2ad
Madhvi Sharma <sharmamadhvi347@gmail.com>
2026-04-26 19:18:58 +0530

    UC2: Inches equality implemented

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -1,7 +1,10 @@
 package com.apps.quantitymeasurement;
 
+import java.util.Objects;
+
 public class QuantityMeasurementApp {
 
+    // Feet Class
     public static class Feet {
         private final double value;
 
@@ -16,6 +19,34 @@ public class QuantityMeasurementApp {
 
             Feet other = (Feet) obj;
             return Double.compare(this.value, other.value) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(value);
+        }
+    }
+
+    // Inches Class
+    public static class Inches {
+        private final double value;
+
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+
+            Inches other = (Inches) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(value);
         }
     }
 }
--- src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
+++ src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -3,34 +3,87 @@ package com.apps.quantitymeasurement;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+// Import inner classes properly
+import com.apps.quantitymeasurement.QuantityMeasurementApp.Feet;
+import com.apps.quantitymeasurement.QuantityMeasurementApp.Inches;
+
 public class QuantityMeasurementAppTest {
+
+    // -------- FEET TESTS --------
+
+    @Test
+    void testFeetEquality_SameValue() {
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
+
+        assertTrue(f1.equals(f2));
+    }
 
     @Test
     void testFeetEquality_DifferentValue() {
-        var f1 = new QuantityMeasurementApp.Feet(1.0);
-        var f2 = new QuantityMeasurementApp.Feet(2.0);
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(2.0);
 
         assertFalse(f1.equals(f2));
     }
 
     @Test
-    void testFeetEquality_Null() {
-        var f1 = new QuantityMeasurementApp.Feet(1.0);
+    void testFeetEquality_NullComparison() {
+        Feet f1 = new Feet(1.0);
 
         assertFalse(f1.equals(null));
     }
 
     @Test
-    void testFeetEquality_DifferentType() {
-        var f1 = new QuantityMeasurementApp.Feet(1.0);
+    void testFeetEquality_DifferentClass() {
+        Feet f1 = new Feet(1.0);
 
         assertFalse(f1.equals("test"));
     }
 
     @Test
     void testFeetEquality_SameReference() {
-        var f1 = new QuantityMeasurementApp.Feet(1.0);
+        Feet f1 = new Feet(1.0);
 
         assertTrue(f1.equals(f1));
+    }
+
+    // -------- INCHES TESTS --------
+
+    @Test
+    void testInchesEquality_SameValue() {
+        Inches i1 = new Inches(1.0);
+        Inches i2 = new Inches(1.0);
+
+        assertTrue(i1.equals(i2));
+    }
+
+    @Test
+    void testInchesEquality_DifferentValue() {
+        Inches i1 = new Inches(1.0);
+        Inches i2 = new Inches(2.0);
+
+        assertFalse(i1.equals(i2));
+    }
+
+    @Test
+    void testInchesEquality_NullComparison() {
+        Inches i1 = new Inches(1.0);
+
+        assertFalse(i1.equals(null));
+    }
+
+    @Test
+    void testInchesEquality_DifferentClass() {
+        Inches i1 = new Inches(1.0);
+
+        assertFalse(i1.equals("test"));
+    }
+
+    @Test
+    void testInchesEquality_SameReference() {
+        Inches i1 = new Inches(1.0);
+
+        assertTrue(i1.equals(i1));
     }
 }
