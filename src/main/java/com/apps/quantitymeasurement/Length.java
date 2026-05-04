6541271705aa1efa5a235b4983a91220ca5a8748
Madhvi Sharma <sharmamadhvi347@gmail.com>
2026-05-04 22:47:36 +0530

    UC6: Add addition of length units

--- src/main/java/com/apps/quantitymeasurement/Length.java
+++ src/main/java/com/apps/quantitymeasurement/Length.java
@@ -23,24 +23,62 @@ public class Length {
     }
 
     public Length(double value, LengthUnit unit) {
-        if (unit == null) {
-            throw new IllegalArgumentException("Unit cannot be null");
+        if (unit == null || !Double.isFinite(value)) {
+            throw new IllegalArgumentException("Invalid length");
         }
         this.value = value;
         this.unit = unit;
     }
 
-    private double toInches() {
-        return value * unit.getConversionFactor();
+    private double convertToBaseUnit() {
+        return this.value * this.unit.getConversionFactor();
+    }
+
+    private boolean compare(Length that) {
+        double thisInches = this.convertToBaseUnit();
+        double thatInches = that.convertToBaseUnit();
+        return Math.abs(thisInches - thatInches) < 0.01;
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Length)) return false;
+        Length that = (Length) o;
+        return compare(that);
+    }
 
-        Length other = (Length) obj;
+    public Length convertTo(LengthUnit targetUnit) {
+        if (targetUnit == null) {
+            throw new IllegalArgumentException("Target unit cannot be null");
+        }
+        double inches = convertToBaseUnit();
+        double converted = inches / targetUnit.getConversionFactor();
+        return new Length(round(converted), targetUnit);
+    }
 
-        return Math.abs(this.toInches() - other.toInches()) < 0.0001;
+    // 🔥 UC6 ADD METHOD
+    public Length add(Length that) {
+        if (that == null) {
+            throw new IllegalArgumentException("Cannot add null");
+        }
+
+        double thisInches = this.convertToBaseUnit();
+        double thatInches = that.convertToBaseUnit();
+
+        double sumInches = thisInches + thatInches;
+
+        double resultValue = sumInches / this.unit.getConversionFactor();
+
+        return new Length(round(resultValue), this.unit);
+    }
+
+    private double round(double value) {
+        return Math.round(value * 100.0) / 100.0;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%.2f %s", value, unit);
     }
 }
--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -6,35 +6,35 @@ public class QuantityMeasurementApp {
         return l1.equals(l2);
     }
 
-    public static void demonstrateLengthComparison(double v1, Length.LengthUnit u1,
-                                                   double v2, Length.LengthUnit u2) {
-
+    public static boolean demonstrateLengthComparison(double v1, Length.LengthUnit u1,
+                                                      double v2, Length.LengthUnit u2) {
         Length l1 = new Length(v1, u1);
         Length l2 = new Length(v2, u2);
+        return l1.equals(l2);
+    }
 
-        System.out.println("Comparing: " + v1 + " " + u1 + " and " + v2 + " " + u2);
-        System.out.println("Equal? " + l1.equals(l2));
-        System.out.println("--------------------------------");
+    public static Length demonstrateLengthConversion(double value,
+                                                     Length.LengthUnit from,
+                                                     Length.LengthUnit to) {
+        return new Length(value, from).convertTo(to);
+    }
+
+    public static Length demonstrateLengthConversion(Length length,
+                                                     Length.LengthUnit to) {
+        return length.convertTo(to);
+    }
+
+    // 🔥 UC6 METHOD
+    public static Length demonstrateLengthAddition(Length l1, Length l2) {
+        return l1.add(l2);
     }
 
     public static void main(String[] args) {
+        Length l1 = new Length(1.0, Length.LengthUnit.FEET);
+        Length l2 = new Length(12.0, Length.LengthUnit.INCHES);
 
-        demonstrateLengthComparison(1.0, Length.LengthUnit.FEET,
-                12.0, Length.LengthUnit.INCHES);
+        Length result = demonstrateLengthAddition(l1, l2);
 
-        demonstrateLengthComparison(1.0, Length.LengthUnit.YARDS,
-                3.0, Length.LengthUnit.FEET);
-
-        demonstrateLengthComparison(1.0, Length.LengthUnit.YARDS,
-                36.0, Length.LengthUnit.INCHES);
-
-        demonstrateLengthComparison(100.0, Length.LengthUnit.CENTIMETERS,
-                39.3701, Length.LengthUnit.INCHES);
-
-        demonstrateLengthComparison(3.0, Length.LengthUnit.FEET,
-                1.0, Length.LengthUnit.YARDS);
-
-        demonstrateLengthComparison(30.48, Length.LengthUnit.CENTIMETERS,
-                1.0, Length.LengthUnit.FEET);
+        System.out.println("Result: " + result); // 2.00 FEET
     }
 }
